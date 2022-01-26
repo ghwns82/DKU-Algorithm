5095baeda0942a54c61bbd09b5ccf8365fec0a74
H-ChanY <71515744+H-ChanY@users.noreply.github.com>
2022-01-26 16:00:23 +0900

    Create Heo98.py

---
+++ BOJ/1181/Heo98.py
@@ -0,0 +1,15 @@
+# 짧은순으로
+# 사전순
+num=int(input())
+dictionary_list=[]
+for i in range(num):
+    k= input()
+    if k not in dictionary_list:
+        dictionary_list.append(k)
+
+dictionary_list.sort()
+dictionary_list.sort(key = lambda x : len(x))
+
+for i in dictionary_list:
+    print(i)
+    
