006960e49ecc5e0aa26f97ba373614e7818be6cf
HyeonJeong <67951802+12hyeon@users.noreply.github.com>
2022-01-31 17:01:18 +0900

    수정

--- BOJ/1018/hyeon.py
+++ BOJ/1018/hyeon.py
@@ -17,23 +17,3 @@ for h in range(H - 7):  # 가로 j ~ j+7
         else:
             wrongs.append(wrong)
 print(min(wrongs))
-
-
-
-# 방법2) 백준 상에서는 메모리와 시간이 동일
-'''
-answer = 32
-for h in range(H - 7):  # 가로 j ~ j+7
-    for w in range(W - 7):  # 세로 : i ~ i+7
-        wrong = 0
-        for m in range(8):
-            for n in range(8):
-                if board[w+m][h+n] != pattern[m%2][n]:
-                    wrong += 1
-        # 오류 개수를 모았다가 min() 처리보다 빠르게
-        if wrong < 32 and wrong < answer:
-            answer = wrong
-        elif (64-wrong) < answer:
-            answer = 64-wrong
-print(answer)
-'''
