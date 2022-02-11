31c3293751cdb122e6ecb0a23411b705ce6d06d0
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2022-02-11 16:18:07 +0900

    18111 마인크래프트

---
+++ BOJ/18111/ghwns82.py
@@ -0,0 +1,20 @@
+# 18111
+# 성공
+n, m, b = map(int, input().split())
+yard = []
+for _ in range(n):
+    yard += list(map(int, input().split()))
+ans = 10000000000000000000000
+max_h = 0
+for i in range(min(yard), (sum(yard)+ b)//(n*m)+1):
+    result = 0
+    for j in yard:
+        if j>i:
+            result+=(j-i)*2
+        else:
+            result+= (i-j)    
+    if ans>=result:
+        ans = result
+        max_h=i
+        
+print(ans, max_h)
