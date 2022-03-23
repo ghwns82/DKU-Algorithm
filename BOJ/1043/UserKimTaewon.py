ab940459cda4265ce5ebd335747445c7a774f90d
UserKimTaewon <102038119+UserKimTaewon@users.noreply.github.com>
2022-03-23 20:16:30 +0900

    오타 수정

--- BOJ/1043/UserKimTaewon.py
+++ BOJ/1043/UserKimTaewon.py
@@ -28,7 +28,7 @@ def main():
         #    if not trues.isdisjoint(p):
         #        false_parties.remove(p)
         #        newtrues.update(p)
-        # 왠지 모르게 이걸 이용하여 false_parties를 제거할 때는 문제가 틀리다 get_filter와 filter를 사용하니 문제가 풀렸다. 대체 왜?
+        # 왠지 모르게 이걸 이용하여 false_parties에서 거짓말을 할 수 없는 파티를 제거할 때는 문제가 틀리다 get_filter와 filter를 사용하니 문제가 풀렸다. 대체 왜?
         f=get_filter(trues,newtrues)
         false_parties=tuple(filter(f,false_parties))
         trues=newtrues-trues
