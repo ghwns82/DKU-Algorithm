6dc5e4b3c3c5558a63953ecf43a150fec5b4a669
MoonSoJung <94886270+MoonSoJung@users.noreply.github.com>
2022-05-24 02:52:10 +0900

    Create MOON.py

---
+++ BOJ/2822/MOON.py
@@ -0,0 +1,16 @@
+score = []
+for i in range(8):
+    score.append(int(input()))
+score_sort = sorted(score, reverse=True)
+big = []
+for i in range(5):
+    big.append(score_sort[i])
+sum = 0
+tmp = []
+for i in big:
+    sum += i
+    tmp.append(score.index(i))
+print(sum)
+tmp_s = sorted(tmp)
+for i in tmp_s:
+    print(i + 1, end=' ')
