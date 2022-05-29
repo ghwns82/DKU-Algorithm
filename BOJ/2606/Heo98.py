b3462efd9c9ead0b40c4c79cd0baa28c080246e4
CHAN YONG <71515744+H-ChanY@users.noreply.github.com>
2022-05-29 16:48:27 +0900

    Update Heo98.py

--- BOJ/2606/Heo98.py
+++ BOJ/2606/Heo98.py
@@ -2,21 +2,18 @@ import sys
 input=sys.stdin.readline
 n = int(input())
 t = int(input())
-network_ = [[0] * n for i in range(n)]
+s = [[0] * n for i in range(n)]
 visit = [0 for i in range(n)]
-
 for i in range(t):
     a, b = map(int, input().split())
-    network_[a - 1][b - 1] = 1
-    network_[b - 1][a - 1] = 1
-    
+    s[a - 1][b - 1] = 1
+    s[b - 1][a - 1] = 1
 def dfs(v):
     visit[v] = 1
     for i in range(n):
-        if network_[v][i] == 1 and visit[i] == 0:
+        if s[v][i] == 1 and visit[i] == 0:
             dfs(i)
 dfs(0)
-
 cnt = 0
 for i in range(1, n):
     if visit[i] == 1:
